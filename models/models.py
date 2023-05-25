5ff7d6e7d4ea6b12d2dbf937295c3b943a110ed2
llokki <llokki@github.com>
2023-05-25 23:28:52 +0300

    authentification lesson

--- models/models.py
+++ models/models.py
@@ -1,23 +1,26 @@
-from sqlalchemy import MetaData, Table, Column, Integer, String, TIMESTAMP, ForeignKey, JSON, create_engine, event
+from sqlalchemy import MetaData, Table, Column, Integer, Boolean, String, TIMESTAMP, ForeignKey, JSON, create_engine, event
 from datetime import datetime
 
 metadata = MetaData()
 
-roles = Table(
-    "roles",
+role = Table(
+    "role",
     metadata, #для миграции
     Column("id", Integer, primary_key=True),
     Column("name", String, nullable=False),
     Column("permission", JSON),
 )
 
-users = Table(
-    "users",
+user = Table(
+    "user",
     metadata,
     Column("id", Integer, primary_key=True),
     Column("email", String,nullable=False),
     Column("username", String, nullable=False),
-    Column("password", String, nullable=False),
+    Column("hashed_password", String, nullable=False),
     Column("registered_at", TIMESTAMP, default=datetime.utcnow),#не utcnow(), чтобы каждый раз заново вызывалась функция
-    Column("role_id", Integer, ForeignKey("roles.id")),
+    Column("role_id", Integer, ForeignKey(role.c.id)),
+    Column("is_active", Boolean, default=False, nullable=False),
+    Column("is_superuser", Boolean, default=False, nullable=False),
+    Column("is_verified", Boolean, default=True, nullable=False)
 )
